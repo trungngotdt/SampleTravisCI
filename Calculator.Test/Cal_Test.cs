fe0deb1aa1cff2f5f584f6557f4388dad241dc85
trungngotdt <trungngotdt@outlook.com.vn>
2017-10-03 11:50:07 +0700

    version 1.0

---
+++ Calculator.Test/Cal_Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SampleTravisCI;
+
+namespace Calculator.Test
+{
+    [TestFixture]
+    public class Cal_Test
+    {
+        private SampleTravisCI.Calculator _Calculator;
+        [SetUp]
+        public void Setup()
+        {
+            _Calculator = new SampleTravisCI.Calculator();
+        }
+        [Test]
+        public void OnePlusOne()
+        {
+            Assert.AreEqual(2, _Calculator.Add(1, 1));
+        }
+        [Test]
+        public void ThreeSubTwo()
+        {
+            Assert.AreEqual(1, _Calculator.Subtract(3, 2));
+        }
+    }
+}
---
+++ SampleTravisCI/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTravisCI
+{
+    public class Calculator
+    {
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
